c2207a6f4d363850f2a2078a8416546b98012272
Ajlag <40441745+Ajlag@users.noreply.github.com>
2020-01-29 16:22:38 +0100

    Add files via upload

---
+++ arhiva.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GYM.Klase;
+
+namespace GYM
+{
+    public partial class arhiva : UserControl
+    {
+        GymNEntities1 vezasaBazom;
+        public arhiva()
+        {
+            InitializeComponent();
+            vezasaBazom = new GymNEntities1();
+            popuniTabeluArhiva();
+            this.Dock = DockStyle.Fill;
+        }
+
+        private void popuniTabeluArhiva()
+        {
+            var termini = Bazaa.popuniTabeluarhiva();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = termini;
+        }
+    }
+}
